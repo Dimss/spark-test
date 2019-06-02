4ab7eda01260f03639cf2aa74036a55d2ef58d0b
Dmitry Kartsev <dkartsev@redhat.com>
2019-06-02 15:04:29 +0300

    update test script

--- cmd
+++ cmd
@@ -1,4 +1,14 @@
- bin/spark-submit \
+# RUN locally
+bin/spark-submit \
+    --master k8s://https://192.168.64.11:8443 \
+    --deploy-mode cluster \
+    --name spark-k8s-test \
+    --conf spark.executor.instances=3\
+    --conf spark.kubernetes.container.image=dimssss/spark-py:v2.4.3-v2 \
+    /tmp/sparkapp1.py
+
+# RUN script from HDFS
+bin/spark-submit \
     --master k8s://https://192.168.64.11:8443 \
     --deploy-mode cluster \
     --name spark-k8s-test \
@@ -7,4 +17,4 @@
     /tmp/sparkapp1.py
 
 
-    /opt/entrypoint.sh
+    /Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home
--- sparkapp1.py
+++ sparkapp1.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
-from pyspark import SparkConf, SparkContext
+from pyspark import SparkConf, SparkContext, SQLContext
+from pyspark.sql import SparkSession
+
 from operator import add
 
 app_name = "start-test-app1"
 conf = SparkConf().setAppName(app_name)
 sc = SparkContext()
+sqlContext = SQLContext(sc)
 data = sc.parallelize(list("Hello World"))
 counts = data.map(lambda x: (x, 1)).reduceByKey(add).sortBy(lambda x: x[1], ascending=False).collect()
-for (word, count) in counts:
-    print("{}: {}".format(word, count))
+df = sqlContext.createDataFrame(counts)
+# Save DataFrame into HDFS
+df.write.mode("append").format("json").save("hdfs://172.20.10.5:9000/user/dima/res10.json")
+df.show()
+print("========= Loaded from HDFS =============")
+# Load DataFrame from HDFS1
+sparkSession = SparkSession.builder.appName("example-pyspark-read-and-write").getOrCreate()
+df_load = sparkSession.read.json('hdfs://172.20.10.5:9000/user/dima/res10.json')
+df_load.show()
 sc.stop()
